bd57dc2573fda58809ffe9c647d9526a1a2c9c2d
Devanshs22 <devanshshri100@gmail.com>
2025-06-28 05:57:51 +0530

    Task-3

---
+++ delete_t.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$servername = "localhost";
+$username_db = "root";
+$password = "";
+$dbname = "user_management"; 
+
+$conn = new mysqli($servername, $username_db, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM thoughts WHERE id=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    header("Location: thought.php");
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
